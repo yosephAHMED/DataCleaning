0f80db6c02939d4f4bc4bc4ab5bb6b439e7225fd
YosephAhmed <yosephahmed@aol.com>
2020-11-13 01:05:53 -0500

    Setting up main.py comments for authors and information about the project

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 13 00:53:07 2020
+
+@authors: 
+    Ahmed, Yoseph
+    Dropp, Nicholas
+    Fernando, Nattandige
+    
+Project:
+    Using a dataset from the UCI Machine Learning Repository on whether a patient has heart disease.  
+    You will clean the data and then apply a decision tree to the data.
+    
+Github repo:
+    https://github.com/yosephAHMED/DataCleaning
+"""
+
