d6ad13a3ff146375b9652a004153afd1b3078969
YosephAhmed <yosephahmed@aol.com>
2020-11-22 16:15:55 -0500

    feat: implemented decision tree on the date

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ from sklearn.model_selection import GridSearchCV # this will do cross validation
 from sklearn.metrics import confusion_matrix # this creates a confusion matrix
 from sklearn.metrics import plot_confusion_matrix # draws a confusion matrix
 from sklearn.decomposition import PCA # to perform PCA to plot the data
+from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+from sklearn import metrics #Import scikit-learn metrics module for accuracy calculation
+from sklearn import tree
 
 # 1. set up the panda data frame
 data = pd.read_csv("processed.cleveland.data", header=1)
@@ -123,17 +126,62 @@ print("thal column unique vals: ", data_no_missing['thal'].unique())
 X_encoded = pd.get_dummies(X, columns=['cp', 'restecg', 'slope', 'thal'])
 print("\nX_encoded.head():\n", X_encoded.head())
 
-# 17
+# 17. making y value 0 or 1
 print("\ny.unique(): ", y.unique())
 y_not_zero_idx = y > 0
 y[y_not_zero_idx] = 1
 print("\ny.unique(): ", y.unique())
 
+# 18. decision tree
+feature_cols = list(X_encoded)
 
+X_train, X_test, y_train, y_test = train_test_split(X_encoded, y, test_size=0.2, random_state=1)
+# above yields 70% training and 30% test
 
+# Create Decision Tree classifer object
+clf = DecisionTreeClassifier(criterion = "entropy", max_depth = 3)
+#clf = DecisionTreeClassifier(criterion = "entropy")
 
+# Train Decision Tree Classifer
+clf = clf.fit(X_train,y_train)
 
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
 
+# Model Accuracy
+print ("\n------------------------------------------------------------\n")
+print("Model Accuracy\n")
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print ("Precision ", metrics.precision_score(y_test, y_pred))
+print ("Recall ", metrics.recall_score(y_test, y_pred ))
+print ("f1 score ", metrics.f1_score(y_test, y_pred ))
+print("roc-auc score:", metrics.roc_auc_score(y_test, y_pred))
 
+fig = plt.figure(figsize=(25,20))
+_ = tree.plot_tree(clf,
+                   feature_names=feature_cols, 
+                   class_names=['0','1'],
+                   #class_names=datatext.target_names,
+                   filled=True)
 
+from sklearn.ensemble import RandomForestClassifier
+clf = RandomForestClassifier(max_depth=3, random_state=0)
+clf.fit(X_train,y_train)
+
+#Predict the response for test dataset
+y_pred = clf.predict(X_test)
+
+print ("\n------------------------------------------------------------\n")
+print("Predicted response for test dataset\n")
+print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print ("Precision ", metrics.precision_score(y_test, y_pred))
+print ("Recall ", metrics.recall_score(y_test, y_pred ))
+print ("f1 score ", metrics.f1_score(y_test, y_pred ))
+print("roc-auc score:", metrics.roc_auc_score(y_test, y_pred))
+
+# The tree has four levels including the root node
+# For the tree to come to an accurate decision whether or not
+# the patient has heart disease, we need to traverse
+# a total of 4 levels in the tree. If we don't include the root, 
+# the answer is 3
 
